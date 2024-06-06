e305c3d264e22fbb0c5998bdc30f2d229e235966
Gyanshu Pathak <125213369+gyanshupathak@users.noreply.github.com>
2024-06-06 13:30:56 +0530

    added

--- frontend/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-chmod -R 755 node_modules/.bin
-chmod -R 755 node_modules/react-scripts
-npx react-scripts build
--- frontend/vercel-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-npm install --legacy-peer-deps --force
-chmod -R 755 node_modules/.bin
-chmod -R 755 node_modules/react-scripts
-npm run build
